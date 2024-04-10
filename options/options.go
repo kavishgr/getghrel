549e8f4465b43ea675d13de556b42ee2771f5336
Kavish Gour <kavishgr@protonmail.com>
2024-04-10 20:24:43 +0400

    added colors to option flags

--- options/options.go
+++ options/options.go
@@ -2,7 +2,8 @@ package options
 
 import (
 	"flag"
-	"fmt"
+	// "fmt"
+	"github.com/mitchellh/colorstring"
 	"os"
 	"strings"
 )
@@ -21,21 +22,18 @@ func ParseFlags() options {
 
 	flag.Usage = func() {
 		h := []string{
+			"",
 			"Download releases from github and retain only the binaries",
 			"",
-			"Usage:",
+			"[light_cyan]Usage:[reset]",
+			"",
 			"  echo 'https://github.com/sharkdp/bat' | getghrel -list | sort",
 			"  cat urls.txt | getghrel -list | sort | tee releases.txt",
 			"  cat releases.txt | getghrel -download",
 			"  cat releases.txt | getghrel -download -tempdir '/tmp/bin'",
 			"  cat urls.txt | getghrel -list | grep -vi '^n/a'",
 			" ",
-			"If a repository doesn't have a latest release tag, the tool will print for the latest tag available.",
-			"This can include nightly/unstable releases.\n",
-			"Checksums, SBOMs, and files related to your OS and Arch will also be included.",
-			"You can use `grep` to filter out these results.\n",
-			"Repos that do not have a release section, \n",
-			"The url format for -list: \n",
+			"[light_cyan]The url format for -list[reset]: \n",
 			"  A github url -> 'https://github.com/owner/repo'",
 			"  For e.g 'https://github.com/sharkdp/bat'\n",
 			"  Or only owner and repository -> 'owner/repo'",
@@ -43,36 +41,53 @@ func ParseFlags() options {
 			"For more examples, browse to: https://github.com/kavishgr/getghrel",
 			"",
 			"Options:",
-			"  -list list all the releases found",
+			"  [light_cyan]-list[reset]",
+			"",
 			"\tWill list all the release/releases found for your OS and Architecture.\n",
 			"\tExample: cat urls.txt | getghrel -list | sort",
 			"\tExample: echo 'https://github.com/sharkdp/bat' | getghrel -list | sort",
+			"\tExample: echo 'sharkdp/bat' | getghrel -list | sort",
 			"",
-			"  -con <int> set the concurrency level (default: 2)\n",
-			"\t Example: cat urls.txt | getghrel -list -con 3",
+			"  [light_cyan]-con[reset]",
+			"",
+			"\t Set the concurrency level (default: 2)\n",
+			"\t Example: cat urls.txt | getghrel -list -con 3 | tee releases.txt",
 			"\t Example: cat releases.txt | getghrel -download -con 3",
 			"",
-			"  -ghtoken <string> specify you GITHUB TOKEN",
+			"  [light_cyan]-ghtoken[reset]",
+			"",
+			"\t Specify your GITHUB TOKEN",
 			"\t Default is the GIHUB_TOKEN environment variable.\n",
 			"\t Example: cat urls.txt | getghrel -list -ghtoken 'YOUR TOKEN'",
 			"",
-			"  -download download the releases",
-			"\tDefault directory in which the release will be downloaded is '/tmp/getghrel'",
-			"\tIf the release is compressed or in an archive format, the tool will automatically extract and unpack it",
-			"\tno matter how it's compressed, and keep only the binary.\n",
-			"\tExample: cat urls_from_list_results.txt | getghrel -download",
-			"\tExample: cat urls_from_list_results.txt | getghrel -download -tempdir '/tmp/test'",
+			"  [light_cyan]-download[reset]",
 			"",
-			"  -skipextraction skip the extraction process",
+			"\t Download the releases",
+			"\t Default directory in which the release will be downloaded is '/tmp/getghrel'",
+			"\t If the release is compressed or in an archive format, the tool will automatically", 
+			"\t extract and unpack it no matter how it's compressed or archived",
+			"\t and keep only the binary.\n",
+			"\t Example: cat releases.txt | getghrel -download",
+			"\t Example: cat releases.txt | getghrel -download -tempdir '/tmp/test'",
+			"",
+			"  [light_cyan]-skipextraction[reset]",
+			"",
+			"\t Skip the extraction/unpack process\n",
 			"\t Example: echo \"neovim/neovim\" | getghrel -list | getghrel -download -skipextraction",
 			"",
-			"  -tempdir <string> specify a temporary directory to download/extract the binaries\n",
-			"\tExample: cat urls_from_list_results.txt | getghrel -download -tempdir '/tmp/test'",
+			"  [light_cyan]-tempdir[reset] ",
+			"",
+			"\t Specify a temporary directory to download/extract the binaries\n",
+			"\t Example: cat releases.txt | getghrel -download -tempdir '/tmp/test'",
+			"",
+			"  [light_cyan]-version[reset]",
+			"\t Print version\n",
 			"",
 		}
+		help := strings.Join(h, "\n")
 
 		// fmt.Fprint(os.Stderr, strings.Join(h, "\n"))
-		fmt.Fprint(os.Stdout, strings.Join(h, "\n"))
+		colorstring.Println(help)
 	}
 
 	opts := options{}
